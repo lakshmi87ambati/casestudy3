52539694600e9f02675d1a0d6a2e4d3f315622b6
lakshmi87ambati <lt.ambati@gmail.com>
2021-02-13 14:30:45 +0000

    FIRST COMMIT

---
+++ com.MyTest/src/test/java/com/MavenTest/AppLoginTest.java
@@ -0,0 +1,47 @@
+package com.MavenTest;
+
+
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import com.MavenTest.Utilities.BrowserFactory;
+import com.MavenTest.Utilities.CaptureScreens;
+import com.MavenTest.Utilities.OrangePageObjects;
+
+
+public class AppLoginTest 
+{
+	public static WebDriver driver;
+	public static String weburl="https://opensource-demo.orangehrmlive.com/index.php/auth/login";
+	public static BrowserFactory browser=new BrowserFactory();
+	public static OrangePageObjects loginpage=new OrangePageObjects(driver);
+	public static CaptureScreens screens=new CaptureScreens();
+    
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	
+    	driver=BrowserFactory.getbrowser("firefox",weburl);
+		loginpage=new OrangePageObjects(driver);
+		OrangePageObjects.getusername("Admin");
+		
+		OrangePageObjects.getpassword("admin123");
+		
+		OrangePageObjects.login();
+		
+		OrangePageObjects.clickleavebutton();
+		OrangePageObjects.clickAssignLeave();
+		//CaptureScreens.ScreenCapture(driver,"Assign Leave Page");
+		OrangePageObjects.enteremplyeename();
+		OrangePageObjects.leavetype();
+		OrangePageObjects.leaveFromdate();
+		OrangePageObjects.leaveTodate();
+		OrangePageObjects.comment();
+		OrangePageObjects.clickAssignButton();
+		
+		
+		driver.close();
+        
+    }
+}
